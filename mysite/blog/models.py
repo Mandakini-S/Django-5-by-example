c4786a59bda3e729ba6667df8b8e85f9014347f6
Mandakini Sapkota <mandakinisapkota@gmail.com>
2024-10-05 21:40:22 +0545

    Make SEO friendly url

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.db import models
+from django.urls import reverse
 from django.utils import timezone
 from django.contrib.auth.models import User
 
@@ -30,6 +31,17 @@ class Post(models.Model):
    max_length=10, 
    choices=Status.choices,
    default=Status.DRAFT)
+ 
+def get_absolute_url(self):
+   return reverse(
+ 'blog:post_detail',
+   args=[
+ self.publish.year,
+ self.publish.month,
+ self.publish.day,
+ self.slug
+   ]
+   )
  
 objects = models.Manager() # The default manager.
 published = PublishedManager()
